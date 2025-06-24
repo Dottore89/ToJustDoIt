3404e5f187876dca8dfd0980c4cef16467177361
sergio <115156475+Dottore89@users.noreply.github.com>
2025-06-24 14:24:54 +0200

    Refactor ToDoList and remove Title component
    Moved the title and add-task button from the Title component into ToDoList, deleted the Title component, and updated ToDoList to handle task addition state. Also updated the main app to use only ToDoList and adjusted the page background color in index.html.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import Title from "./components/Title";
 import ToDoList from "./components/ToDoList";
 
 export default function App() {
   return (
     <>
-      <Title />
       <ToDoList />
     </>
   )
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,8 +1,51 @@
-export default function ToDoList() {
+import { Plus } from 'lucide-react'
+import { useState } from 'react'
+
+
+export default function Title() {
+    const [tasksArr, setTasksArr] = useState([])
+    const [addingTask, setAddingTask] = useState(false)
+
+    function addTask() {
+        //open modal
+        //input name of task
+        //add task to tasksArr
+        //render task into ToDo list
+    }
+
+    function removeTask() {
+        //
+    }
+
     return (
-        <div className="flex flex-col items-center border-3">
-            I'm the ToDo list
-            <div>I'm ToDo task number 1</div>
-        </div>
+        <>
+            <div className="flex gap-4 justify-center items-center my-8">
+                <div className="font-extrabold text-4xl">JUST DO IT! LIST</div>
+                <div className="bg-red-500 rounded-full text-white cursor-pointer hover:bg-yellow-500 p-2 transition-colors">
+                    <Plus size={45} strokeWidth={3} onClick={() => setAddingTask( prevState => !prevState)} />
+                </div>
+            </div>
+
+
+            <div className="flex flex-col items-center">
+                I'm the ToDo list
+                <div>I'm ToDo task number 1</div>
+            </div>
+            {/* 
+                
+
+            */}
+            {addingTask ?
+                <div className='flex justify-center mt-12 fixed bottom-10 transform -translate-x-1/2 left-1/2 text-2xl md:text-3xl lg:text-4xl'>
+                    <input
+                        type='text' 
+                        placeholder='Add task...' 
+                        className='bg-[#F5EBDD] shadow-2xl rounded-md py-4 px-8 focus:outline-none focus:ring-2 focus:ring-yellow-400' 
+                    />
+                </div>
+                :
+                null
+            }
+        </>
     )
 }
